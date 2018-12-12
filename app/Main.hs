26de8306e2969d438ba23ec80bad811de5928a2d
Oren <oren@atidot.com>
2018-12-12 10:29:38 +0200

    added timeit timer which not seems to do exactly what I want

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,13 @@ import System.Environment               (getArgs)
 import System.Process                   (readCreateProcess, shell)
 import Data.List.Split                  (splitOn)
 import Control.Monad                    (forever)
+import Control.Monad.IO.Class           (MonadIO)
 import Control.Concurrent               (threadDelay)
 import Control.Exception                (evaluate)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node (initRemoteTable, runProcess)
 import Control.Distributed.Process.Backend.SimpleLocalnet
+import System.TimeIt                    (timeItT)
 
 import qualified MasterSlave as MS
 
@@ -27,7 +29,7 @@ main = do
             --------
             backend <- initializeBackend hostIP port rtable
             startMaster backend $ \slaves -> forever $ do
-                result <- MS.builder (read n, read m) slaves
+                result <- timer $ MS.builder (read n, read m) slaves
                 liftIO $ hPrint stderr result
                 liftIO $ threadDelay (10 ^ 6)
         ["worker", port] -> do
@@ -35,3 +37,9 @@ main = do
             backend <- initializeBackend hostIP port rtable
             startSlave backend
         _ -> hPrint stderr "Error, Bad Args"
+
+timer :: (MonadIO m, Show a) => m a -> m a
+timer ioa = do
+    (t, a) <- timeItT ioa
+    liftIO $ hPrint stderr t
+    return a
